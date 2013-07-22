9e33de0b24c365265a54a31846aaa5b8bb57c1d1
Roy Simkes <roysimkes@gmail.com>
2013-07-22 23:50:45 +0300

    added total frame count

--- jquery.comics.js
+++ jquery.comics.js
@@ -1,12 +1,15 @@
 /*
+// FIXME: Cancel the transition effect if button is pressed again
 // TODO: Show the full page function...
 // TODO: Add Frame Editor...
 // TODO: Mobile Add touch controls...
 // TODO: Disable buttons when on end?
-// TODO: POsitional Click Events
+// TODO: Positional Click Events
+// TODO: Read the comic directly from a folder (auto parse source.js file or something...)
+// TODO: Instead of total pages, show total frames...
 */
 $.fn.comics = function(comic, withControls) {
-  this.pages = comic.pages, this.currentPageIndex = 0, this.currentFrameIndex = 0, this.defaultTransitionSpeed = 1000, this.title = comic.title, this.path = "";
+  this.pages = comic.pages, this.currentPageIndex = 0, this.currentFrameIndex = 0, this.defaultTransitionSpeed = 1000, this.title = comic.title, this.path = "", this.totalFrames = 0;
   if (!!comic.sourcePath) {
     this.path = comic.sourcePath;
     if (!this.path.endsWith("/")) {
@@ -26,6 +29,7 @@ $.fn.comics = function(comic, withControls) {
             "outline": 0
           })
       );
+      self.totalFrames += el.frames.length;
     });
     this.css("overflow", "hidden");
     if (withControls) {
